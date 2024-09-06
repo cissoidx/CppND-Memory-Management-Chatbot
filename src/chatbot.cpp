3a8fa5152eae5e8a24f383a1dd394b8b34e36f1d
xu dong <xu.dong.gx@renesas.com>
2024-09-06 22:25:52 +0800

    first version, does not work, but I cannot find problems

--- src/chatbot.cpp
+++ src/chatbot.cpp
@@ -45,6 +45,57 @@ ChatBot::~ChatBot()
 //// STUDENT CODE
 ////
 
+ChatBot::ChatBot(const ChatBot &source)
+{
+    std::cout << "ChatBot Copy Constructor" << std::endl;
+    _image = new wxBitmap();
+    *_image = *source._image;
+    _chatLogic = source._chatLogic;
+    _rootNode = source._rootNode;
+}
+
+ChatBot & ChatBot::operator=(const ChatBot &source)
+{
+    std::cout << "ChatBot Copy Assignment Operator" << std::endl;
+    if (this == &source)
+        return *this;
+    delete _image;
+    _image = new wxBitmap();
+    *_image = *source._image;
+    _chatLogic = source._chatLogic;
+    _rootNode = source._rootNode;
+    return *this;
+}
+
+ChatBot::ChatBot(ChatBot &&source)
+{
+    std::cout << "ChatBot Move Constructor" << std::endl;
+    _image = source._image;
+    source._image = NULL;
+
+    _chatLogic = source._chatLogic;
+    source._chatLogic = nullptr;
+
+    _rootNode = source._rootNode;
+    source._rootNode = nullptr;
+}
+
+ChatBot & ChatBot::operator=(ChatBot &&source)
+{
+    std::cout << "ChatBot Move Assignment Operator" << std::endl;
+    if (this == &source)
+        return *this;
+    delete _image;
+    _image = source._image;
+    source._image = NULL;
+
+    _chatLogic = source._chatLogic;
+    source._chatLogic = nullptr;
+
+    _rootNode = source._rootNode;
+    source._rootNode = nullptr;
+    return *this;
+}
 ////
 //// EOF STUDENT CODE
 
